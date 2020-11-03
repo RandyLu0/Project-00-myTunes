a65ebe986c763119802c143488321be5ca38cc42
RandyLu0 <lurandy8@gmail.com>
2020-11-03 17:44:56 -0500

    renamed random function to shuffle

--- playlist.h
+++ playlist.h
@@ -14,7 +14,7 @@ struct playlist* insert_alpha(struct playlist* s, char* n, char* a);
 void print(struct playlist* s);
 struct playlist* find_song(struct playlist* s, char* n, char* a);
 struct playlist* find_artist(struct playlist* s, char* a);
-struct playlist* random(struct playlist* s);
+struct playlist* shuffle(struct playlist* s);
 struct playlist* remove(struct playlist* s, char* n, char* a);
 struct playlist* free_playlist(struct playlist* s);
 
