587fe2dc69a4defc65feda60cd3069f0fc4db130
Devin Zhang <71523929+dzhang10@users.noreply.github.com>
2020-11-06 21:29:45 -0500

    Update playlistarray.c

--- playlistarray.c
+++ playlistarray.c
@@ -27,8 +27,56 @@ struct playlist* add_playlist(struct playlist ** lib, char* n, char* a){
 
 }
 
-struct playlist* search_song(struct playlist* s, char* n, char* a);
+struct playlist* search_song(struct playlist ** lib, char* n, char* a){
+
+        char first_letter =((lib[0])->artist)[0];
+        int i=-1;
+
+        if(first_letter-'a' >= 0 && first_letter-'a' <= 25){
+                i = first_letter-'a';
+        }
+        else
+                if(first_letter-'A' >= 0 && first_letter-'A' <= 25){
+                        i = first_letter-'A';
+                }
+        else
+                i=27;
+
+        struct playlist* node = lib[i];
+        while(1){
+                if(strcmp(node->name,n)==0 && strcmp(node->artist,a)==0){
+                        return node;
+                }
+                node=node->next;
+        }
+
+        return NULL;
+}
 
 
+struct playlist* search_artist(struct playlist ** lib, char* a){
 
-struct playlist* search_artist(struct playlist* s, char* a);
+
+        char first_letter =((lib[0])->artist)[0];
+        int i=-1;
+
+        if(first_letter-'a' >= 0 && first_letter-'a' <= 25){
+                i = first_letter-'a';
+        }
+        else
+                if(first_letter-'A' >= 0 && first_letter-'A' <= 25){
+                        i = first_letter-'A';
+                }
+        else
+                i=27;
+
+        struct playlist* node = lib[i];
+        while(1){
+                if(strcmp(node->artist,a)==0){
+                        return node;
+                }
+                node=node->next;
+        }
+
+        return NULL;
+}
