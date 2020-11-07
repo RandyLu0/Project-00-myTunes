ed2c34133f6698959aa41f9865a8baeed882d32f
Devin Zhang <71523929+dzhang10@users.noreply.github.com>
2020-11-06 21:09:36 -0500

    Update and rename library.h to playlistarray.h

---
+++ playlistarray.h
@@ -0,0 +1,16 @@
+#ifndef PLAYLISTARRAY_H
+#define PLAYLISTARRAY_H
+
+struct playlist;
+
+struct playlist* add_playlist(struct playlist ** lib, char* n, char* a);
+struct playlist* search_song(struct playlist ** lib, char* n, char* a);
+struct playlist* search_artist(struct playlist ** lib, char* a);
+void print_alpha(struct playlist ** lib, char* letter);
+void print_artist(struct playlist ** lib, char* a);
+void print_library(struct playlist ** lib);
+void shuffle(struct playlist ** lib);
+void delete(struct playlist ** lib, char* n, char* a);
+void clear(struct playlist ** lib);
+
+#endif
