523ba5841da6fd232a1f6e578d454a11981042d2
gzhao10 <68718446+gzhao10@users.noreply.github.com>
2020-11-08 14:47:40 -0500

    Update playlistarray.h

--- playlistarray.h
+++ playlistarray.h
@@ -7,7 +7,7 @@ struct playlist* add_playlist(struct playlist ** lib, char*n , char *a);
 struct playlist* search_song(struct playlist** lib, char* n, char* a);
 struct playlist* search_artist(struct playlist** lib, char* a);
 
-void print_alpha(struct playlist ** lib, char* letter);
+void print_alpha(struct playlist ** lib, char letter);
 void print_artist(struct playlist ** lib, char* a);
 void print_library(struct playlist ** lib);
 void shuffle(struct playlist ** lib);
